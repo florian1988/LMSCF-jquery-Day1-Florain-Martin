6ed4c7aafa062e01abe66dd909d01a418bea6d4d
Marin Balabanov <marincomics@gmail.com>
2020-02-10 14:59:29 +0100

    First attempt at responsiveness

--- 04_grocery/js/custom.js
+++ 04_grocery/js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	for (let i in productsdata.products) {
 		$('#productArea').append(`
-		<div class="itemCard" id="item${productsdata.products[i].id}">
+		<div class="itemCard stocked" id="item${productsdata.products[i].id}">
 			<div class="picSpace">
 				<img src="${productsdata.products[i].image}">
 			</div>
@@ -15,11 +15,11 @@ $(document).ready(function(){
 		`);
 
 		if ( parseInt(productsdata.products[i].amount) > 0 && parseInt(productsdata.products[i].amount) < 4 ) {
-			$('#item' + productsdata.products[i].id).addClass('low');
+			$('#item' + productsdata.products[i].id).removeClass('stocked').addClass('low');
 		}
 
 		if ( parseInt(productsdata.products[i].amount) < 1 ) {
-			$('#item' + productsdata.products[i].id).addClass('soldOut');
+			$('#item' + productsdata.products[i].id).removeClass('stocked').addClass('soldOut');
 		}
 	}
 
