d600e659643464ea9f7d1c77ceb117ca7160bd60
Marin Balabanov <marincomics@gmail.com>
2020-02-10 14:21:09 +0100

    Added Marin's Grocery Store

--- 04_grocery/json/products.js
+++ 04_grocery/json/products.js
@@ -7,6 +7,7 @@ let productsdata = JSON.parse(`{
          "name": "Avocado",
          "info": "Vestibulum id ligula porta felis.",
          "image": "img/avocado.jpg",
+         "amount": "12",
          "price": "3"
        },
        {
@@ -14,6 +15,7 @@ let productsdata = JSON.parse(`{
          "name": "Coffee Beans",
          "info": "Donec sed odio dui. Praesent commodo cursus magna.",
          "image": "img/coffee-beans.jpg",
+         "amount": "5",
          "price": "6"
 
        },
@@ -22,6 +24,7 @@ let productsdata = JSON.parse(`{
          "name": "French Cheese",
          "info": "Maecenas sed diam eget risus varius blandit.",
          "image": "img/french-cheese.jpg",
+         "amount": "3",
          "price": "5"
        },
        {
@@ -29,6 +32,7 @@ let productsdata = JSON.parse(`{
          "name": "Lemon",
          "info": "Aenean eu leo quam. Pellentesque ornare sem.",
          "image": "img/lemon.jpg",
+         "amount": "2",
          "price": "1"
        },
        {
@@ -36,6 +40,7 @@ let productsdata = JSON.parse(`{
          "name": "Lettuce",
          "info": "Lorem ipsum dolor sequitur.",
          "image": "img/lettuce.jpg",
+         "amount": "4",
          "price": "2"
        },
        {
@@ -43,6 +48,7 @@ let productsdata = JSON.parse(`{
          "name": "Mango",
          "info": "Aenean eu leo quam. Pellentesque ornare sem.",
          "image": "img/mango.jpg",
+         "amount": "0",
          "price": "6"
        },
        {
@@ -50,6 +56,7 @@ let productsdata = JSON.parse(`{
          "name": "Milk",
          "info": "Donec sed odio dui. Fusce dapibus tellus.",
          "image": "img/milk.jpg",
+         "amount": "8",
          "price": "2"
        },
        {
@@ -58,6 +65,7 @@ let productsdata = JSON.parse(`{
          "description": "Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.",
          "info": "Lorem Ipsum Dolor",
          "image": "img/orange-juice.jpg",
+         "amount": "2",
          "price": "2"
        },
        {
@@ -65,6 +73,7 @@ let productsdata = JSON.parse(`{
          "name": "Radishes",
          "info": "Maecenas faucibus mollis interdum.",
          "image": "img/radishes.jpg",
+         "amount": "0",
          "price": "3"
        }
     ]
